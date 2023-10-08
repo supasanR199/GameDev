03ed95704b36fa228f43ee0e94f6a178c70ce2d4
Naddanai Parod <naddanaiparod55@gmail.com>
2023-10-08 14:10:20 +0700

    move 8 direction

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,40 +1,32 @@
 extends KinematicBody2D
-var input_direction = 0
-var direction = 0
-var speed = 0
-var velocity = 0
-const MAX_SPEED = 400
-const ACCELERRATION = 700
-const DECELERATION = 600
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var speed = 200  # speed in pixels/sec
+var animated_dir = "_down"
+var status_a = "idel"
 
 func _process(_delta):
-	# Player input handling
-	if input_direction:
-		direction = input_direction
 	if Input.is_action_pressed("ui_left"):
-		input_direction = -1
-		get_node("AnimatedSprite").set_flip_h(true)
-		get_node("AnimatedSprite").play("move_left")
+		status_a = "move"
+		animated_dir = "_left"
+		#get_node("AnimatedSprite").play("move_left")
 	elif Input.is_action_pressed("ui_right"):
-		input_direction = 1
-		get_node("AnimatedSprite").set_flip_h(false)
-		get_node("AnimatedSprite").play("move_right")
+		status_a = "move"
+		animated_dir = "_right"
+		#get_node("AnimatedSprite").play("move_right")
+	elif Input.is_action_pressed("ui_up"):
+		status_a = "move"
+		animated_dir = "_top"
+		#get_node("AnimatedSprite").play("move_top")
+	elif Input.is_action_pressed("ui_down"):
+		status_a = "move"
+		animated_dir = "_down"
+		#get_node("AnimatedSprite").play("move_down")
 	else:
-		input_direction = 0
-		get_node("AnimatedSprite").play("idel")
-	if input_direction == -direction:
-		speed /= 3
-	if input_direction:
-		speed += ACCELERRATION * _delta
-	else:
-		speed -= DECELERATION * _delta
-	speed = clamp(speed,0,MAX_SPEED)
-	velocity = speed * _delta * direction
-	move_and_collide(Vector2(velocity,0))
-	pass
+		status_a = "idel"
+		#get_node("AnimatedSprite").play("idel")
+	get_node("AnimatedSprite").play(status_a + animated_dir )
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	print(direction)
+	move_and_slide(direction * speed)
