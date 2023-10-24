e78a83f6ce700989f82b1713ecc5cea741e05b2d
Naddanai Parod <naddanaiparod55@gmail.com>
2023-10-24 20:09:34 +0700

    add player node

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,33 +1,32 @@
 extends CharacterBody2D
-
-var speed = 200  # speed in pixels/sec
+var speed = 200 
 var animated_dir = "_down"
 var status_a = "idel"
 
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	move_and_slide()
+	
 func _process(_delta):
 	if Input.is_action_pressed("ui_left"):
 		status_a = "move"
 		animated_dir = "_left"
-		#get_node("AnimatedSprite").play("move_left")
 	elif Input.is_action_pressed("ui_right"):
 		status_a = "move"
 		animated_dir = "_right"
-		#get_node("AnimatedSprite").play("move_right")
 	elif Input.is_action_pressed("ui_up"):
 		status_a = "move"
-		animated_dir = "_top"
-		#get_node("AnimatedSprite").play("move_top")
+		animated_dir = "_up"
 	elif Input.is_action_pressed("ui_down"):
 		status_a = "move"
 		animated_dir = "_down"
-		#get_node("AnimatedSprite").play("move_down")
 	else:
 		status_a = "idel"
-		#get_node("AnimatedSprite").play("idel")
+		animated_dir = ""
+	print(status_a + animated_dir)
 	get_node("AnimatedSprite2D").play(status_a + animated_dir )
+	
 
-func _physics_process(delta):
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	print(direction)
-	set_velocity(direction * speed)
-	move_and_slide()
+
+
